47c3322e446d226c3d1acfb277b56385af4e6cb0
Mathieu Roux <mathieu.roux@prestashop.com>
2022-01-05 13:07:40 +0100

    feat: add gulpfile build theme

--- packages/theme/gulpfile.ts
+++ packages/theme/gulpfile.ts
@@ -1,32 +1,39 @@
-import path from 'path';
+import path from 'path'
+import chalk from 'chalk'
 import { src, dest, series, parallel } from 'gulp'
 import gulpSass from 'gulp-sass'
 import dartSass from 'sass'
-import chalk from 'chalk'
 import autoprefixer from 'gulp-autoprefixer'
 import cleanCSS from 'gulp-clean-css'
 import rename from 'gulp-rename'
+import { epOutput } from '../../build/utils/paths'
 
-const distFolder = path.resolve(__dirname, 'dist');
+const distFolder = path.resolve(__dirname, 'dist')
+const distBundle = path.resolve(epOutput, 'theme')
 
-
-function buildTheme() {
+/**
+ * compile theme-chalk scss & minify
+ * not use sass.sync().on('error', sass.logError) to throw exception
+ * @returns
+ */
+function buildThemeChalk() {
   const sass = gulpSass(dartSass)
-
-  const noPuikPrefixFile = /(index)/
+  const noElPrefixFile = /(index)/
   return src(path.resolve(__dirname, 'src/*.scss'))
     .pipe(sass.sync())
-    .pipe(autoprefixer({cascade: false}))
+    .pipe(autoprefixer({ cascade: false }))
     .pipe(
       cleanCSS({}, (details) => {
         console.log(
-          `${chalk.cyan(details.name)}: ${chalk.yellow(details.stats.originalSize / 1000)} KB -> ${chalk.green(details.stats.minifiedSize / 1000)} KB`
+          `${chalk.cyan(details.name)}: ${chalk.yellow(
+            details.stats.originalSize / 1000
+          )} KB -> ${chalk.green(details.stats.minifiedSize / 1000)} KB`
         )
       })
     )
     .pipe(
       rename((path) => {
-        if(!noPuikPrefixFile.test(path.basename)) {
+        if (!noElPrefixFile.test(path.basename)) {
           path.basename = `puik-${path.basename}`
         }
       })
@@ -34,7 +41,26 @@ function buildTheme() {
     .pipe(dest(distFolder))
 }
 
+/**
+ * copy from packages/theme-chalk/lib to dist/theme-chalk
+ */
+export function copyThemeBundle() {
+  return src(`${distFolder}/**`).pipe(dest(distBundle))
+}
 
-export const build = series(buildTheme);
+/**
+ * copy source file to packages
+ */
 
-export default build;
+export function copyThemeSource() {
+  return src(path.resolve(__dirname, 'src/**')).pipe(
+    dest(path.resolve(distBundle, 'src'))
+  )
+}
+
+export const build = parallel(
+  copyThemeSource,
+  series(buildThemeChalk, copyThemeBundle)
+)
+
+export default build
