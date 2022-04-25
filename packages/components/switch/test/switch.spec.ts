6d0304d6f32d137e75bb1065d08a22d577607738
André <andre.amorim@prestashop.com>
2022-04-25 10:18:38 +0200

    test: added unit tests for switch component
    docs: added storybook documentation for switch component

--- packages/components/switch/test/switch.spec.ts
+++ packages/components/switch/test/switch.spec.ts
@@ -5,6 +5,8 @@ import type { MountingOptions, VueWrapper } from '@vue/test-utils'
 
 describe('Switch tests', () => {
   let wrapper: VueWrapper<any>
+  const findLabel = () => wrapper.find('.puik-switch__label')
+  const findSwitch = () => wrapper.find('.puik-switch')
   const factory = (
     propsData: Record<string, any> = {},
     options: MountingOptions<any> = {}
@@ -19,5 +21,27 @@ describe('Switch tests', () => {
   it('should be a vue instance', () => {
     factory()
     expect(wrapper).toBeTruthy()
+  })
+
+  it('should have a label', () => {
+    const label = 'Switch label'
+    factory({ label })
+    expect(findLabel().text()).toBe(label)
+  })
+
+  it('should be disabled', () => {
+    factory({ disabled: true })
+    expect((findSwitch().element as HTMLButtonElement).disabled).toBeTruthy()
+  })
+
+  it('should be set as enabled', () => {
+    factory({ modelValue: true })
+    expect(findSwitch().classes()).toContain('puik-switch--enabled')
+  })
+
+  it('should emit an event on click', async () => {
+    factory({ modelValue: false })
+    await findSwitch().trigger('click')
+    expect(wrapper.emitted('update:modelValue')?.[0]).toStrictEqual([true])
   })
 })
