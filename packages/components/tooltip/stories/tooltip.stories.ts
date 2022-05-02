f9cda2ded35ea306a6c4e466135407b44c643b86
André <andre.amorim@prestashop.com>
2022-05-02 11:44:50 +0200

    docs: added tooltip documentation
    test: added unit test for tooltip

--- packages/components/tooltip/stories/tooltip.stories.ts
+++ packages/components/tooltip/stories/tooltip.stories.ts
@@ -1,18 +1,112 @@
 import Tooltip from './../src/tooltip.vue'
+import { tooltipPosition } from './../src/tooltip'
 import type { Meta, Story, Args } from '@storybook/vue3'
 
 export default {
   title: 'Components/Tooltip',
   component: Tooltip,
+  argTypes: {
+    title: {
+      description: 'Set the tooltip title (also available as a prop)',
+    },
+    description: {
+      description: 'Set the tooltip description (also available as a prop)',
+    },
+    position: {
+      control: 'select',
+      description: 'Set the tooltip position',
+      options: tooltipPosition,
+    },
+  },
+  args: {
+    title: 'Title',
+    description: 'This is a tooltip',
+    position: 'top',
+  },
 } as Meta
 
 const Template: Story = (args: Args) => ({
   components: {
     Tooltip,
   },
-  props: Object.keys(args),
-  template: '<tooltip></tooltip>',
+  setup() {
+    return { args }
+  },
+  template: `
+    <div style="display: flex; align-items: center; justify-content: center; height: 320px;">
+      <tooltip :position="args.position">
+        <template #title>{{ args.title }}</template>
+        <template #description>{{ args.description }}</template>
+      </tooltip>
+    </div>`,
 })
 
 export const Default = Template.bind({})
 Default.args = {}
+
+export const Bottom = () => ({
+  components: {
+    Tooltip,
+  },
+  template: `
+    <div style="display: flex; align-items: center; justify-content: center; height: 320px;">
+      <tooltip position="bottom">
+        <template #title>Title</template>
+        <template #description>This is a bottom tooltip</template>
+      </tooltip>
+    </div>`,
+})
+
+export const Left = () => ({
+  components: {
+    Tooltip,
+  },
+  template: `
+    <div style="display: flex; align-items: center; justify-content: center; height: 320px;">
+      <tooltip position="left">
+        <template #title>Title</template>
+        <template #description>This is a left tooltip</template>
+      </tooltip>
+    </div>`,
+})
+
+export const Right = () => ({
+  components: {
+    Tooltip,
+  },
+  template: `
+    <div style="display: flex; align-items: center; justify-content: center; height: 320px;">
+      <tooltip position="right">
+        <template #title>Title</template>
+        <template #description>This is a right tooltip</template>
+      </tooltip>
+    </div>`,
+})
+
+export const Positions = (args: Args) => ({
+  components: {
+    Tooltip,
+  },
+  setup() {
+    return { args }
+  },
+  template: `
+    <div style="display: flex; align-items: center; justify-content: center; height: 100vh;">
+      <tooltip position="top">
+        <template #title>Title</template>
+        <template #description>This is a top tooltip</template>
+      </tooltip>
+      <tooltip position="bottom">
+        <template #title>Title</template>
+        <template #description>This is a bottom tooltip</template>
+      </tooltip>
+      <tooltip position="left">
+        <template #title>Title</template>
+        <template #description>This is a left tooltip</template>
+      </tooltip>
+      <tooltip position="right">
+        <template #title>Title</template>
+        <template #description>This is a right tooltip</template>
+      </tooltip>
+    </div>`,
+})
--- packages/components/tooltip/test/tooltip.spec.ts
+++ packages/components/tooltip/test/tooltip.spec.ts
@@ -5,6 +5,9 @@ import type { MountingOptions, VueWrapper } from '@vue/test-utils'
 
 describe('Tooltip tests', () => {
   let wrapper: VueWrapper<any>
+  const findTitle = () => wrapper.find('.puik-tooltip__tip__title')
+  const findDescription = () => wrapper.find('.puik-tooltip__tip__description')
+
   const factory = (
     propsData: Record<string, any> = {},
     options: MountingOptions<any> = {}
@@ -16,8 +19,31 @@ describe('Tooltip tests', () => {
       ...options,
     })
   }
+
   it('should be a vue instance', () => {
     factory()
     expect(wrapper).toBeTruthy()
+  })
+
+  it('should have a title and a description', () => {
+    const title = 'Title'
+    const description = 'This is a tooltip'
+    factory(
+      {},
+      {
+        slots: {
+          title,
+          description,
+        },
+      }
+    )
+
+    expect(findTitle().text()).toBe(title)
+    expect(findDescription().text()).toBe(description)
+  })
+
+  it('should be displayed on the right', () => {
+    factory({ position: 'right' })
+    expect(wrapper.find('.puik-tooltip--right').exists()).toBeTruthy()
   })
 })
