6e590f7472760af6e02fd2cedd907a1e1f2f3e7e
ga-devfront <alexis.guyomar@prestashop.com>
2022-07-28 15:18:15 +0200

    feat: add range options

---
+++ packages/components/date-range-picker/src/range-options.ts
@@ -0,0 +1,81 @@
+import {
+  startOfToday,
+  startOfYesterday,
+  subDays,
+  subMonths,
+  startOfMonth,
+  endOfMonth,
+  subYears,
+  startOfYear,
+  endOfYear,
+  startOfWeek,
+  endOfWeek,
+  startOfQuarter,
+  endOfQuarter,
+} from 'date-fns'
+import { useLocale } from '@puik/hooks'
+
+const { t } = useLocale()
+
+const today: Date = startOfToday()
+const yesterday: Date = startOfYesterday()
+const previousMonth: Date = subMonths(today, 1)
+const previousYear: Date = subYears(today, 1)
+
+export default {
+  today: {
+    startDate: today,
+    endDate: today,
+    label: t('puik.dates.today'),
+  },
+  yesterday: {
+    startDate: yesterday,
+    endDate: yesterday,
+    label: t('puik.dates.yesterday'),
+  },
+  last7Days: {
+    startDate: subDays(today, 6),
+    endDate: today,
+    label: t('puik.dates.last7Days'),
+  },
+  last30Days: {
+    startDate: subDays(today, 29),
+    endDate: today,
+    label: t('puik.dates.last30Days'),
+  },
+  last90Days: {
+    startDate: subDays(today, 89),
+    endDate: today,
+    label: t('puik.dates.last90Days'),
+  },
+  lastMonth: {
+    startDate: startOfMonth(previousMonth),
+    endDate: endOfMonth(previousMonth),
+    label: t('puik.dates.lastMonth'),
+  },
+  lastYear: {
+    startDate: startOfYear(previousYear),
+    endDate: endOfYear(previousYear),
+    label: t('puik.dates.lastYear'),
+  },
+  weekToDate: {
+    startDate: startOfWeek(today, { weekStartsOn: 1 }),
+    endDate: endOfWeek(today, { weekStartsOn: 1 }),
+    label: t('puik.dates.weekToDate'),
+  },
+  monthToDate: {
+    startDate: startOfMonth(today),
+    endDate: endOfMonth(today),
+    label: t('puik.dates.monthToDate'),
+  },
+  QuarterToDate: {
+    startDate: startOfQuarter(today),
+    endDate: endOfQuarter(today),
+    label: t('puik.dates.QuarterToDate'),
+  },
+  YearToDate: {
+    startDate: startOfYear(today),
+    endDate: endOfYear(today),
+    label: t('puik.dates.YearToDate'),
+  },
+}
--- packages/locale/lang/en.ts
+++ packages/locale/lang/en.ts
@@ -16,5 +16,18 @@ export default {
       searchPlaceholder: 'Search',
       noResults: 'No results matched',
     },
+    dates: {
+      today: 'Today',
+      yesterday: 'Yesterday',
+      last7Days: 'Last 7 days',
+      last30Days: 'Last 30 days',
+      last90Days: 'Last 90 days',
+      lastMonth: 'Last month',
+      lastYear: 'Last year',
+      weekToDate: 'Week to date',
+      monthToDate: 'Month to date',
+      QuarterToDate: 'Quarter to date',
+      YearToDate: 'Year to date',
+    },
   },
 }
