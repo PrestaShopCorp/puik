67043b1d2d7955695c5bfa57150376420ddb374c
john.R <jonathan_renard@hotmail.fr>
2022-08-08 18:21:20 +0200

    docs: add story
    Signed-off-by: john.R <jonathan_renard@hotmail.fr>

---
+++ packages/components/icon/stories/icon.stories.ts
@@ -0,0 +1,57 @@
+import PuikIcon from './../src/icon.vue'
+import type { Args, Meta, Story } from '@storybook/vue3'
+
+export default {
+  title: 'Components/Icon',
+  component: PuikIcon,
+  argTypes: {
+    icon: {
+      description: 'Set the icon name',
+    },
+    fontSize: {
+      description: 'Set the icon font size',
+      control: {
+        type: 'range',
+        min: 1,
+        max: 100,
+        step: 1,
+      },
+    },
+    color: {
+      description: 'Set the icon color',
+      control: {
+        type: 'color',
+      },
+    },
+  },
+} as Meta
+
+const Template: Story = (args: Args) => ({
+  components: {
+    PuikIcon,
+  },
+  setup() {
+    return { args }
+  },
+  template: `<puik-icon v-bind="args" />`,
+})
+
+export const Default = Template.bind({})
+Default.args = {
+  icon: 'check',
+  color: 'green',
+  fontSize: 2,
+}
+Default.parameters = {
+  docs: {
+    source: {
+      code: `
+      <!--VueJS Snippet-->
+      <puik-icon icon="check" font-size="70px" color="green" />
+
+      <!--HTML/CSS Snippet-->
+      `,
+      language: 'html',
+    },
+  },
+}
