661820eb204ef61c1ecc6fca0c41757734515b6f
Adrien Haik <ady642@gmail.com>
2022-11-29 11:43:48 +0100

    feat: add progress bar component

---
+++ packages/components/progress-bar/src/progress-bar.ts
@@ -0,0 +1,18 @@
+import { buildProps } from '@puik/utils'
+import type { ExtractPropTypes } from 'vue'
+import type ProgressBar from './progress-bar.vue'
+
+export const progressBarProps = buildProps({
+  percentage: {
+    type: [Number, String],
+    required: true,
+  },
+  barClass: {
+    type: String,
+    default: '',
+  },
+} as const)
+
+export type ProgressBarProps = ExtractPropTypes<typeof progressBarProps>
+
+export type ProgressBarInstance = InstanceType<typeof ProgressBar>
---
+++ packages/components/progress-bar/src/progress-bar.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="progress-bar__container">
+    <div
+      :class="barClass"
+      :style="{ width: `${percentage}%` }"
+      class="progress-bar__content"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { progressBarProps } from './progress-bar'
+defineOptions({
+  name: 'PuikProgressBar',
+})
+
+defineProps(progressBarProps)
+</script>
