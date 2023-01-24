73bcbc40c98ed81bc68a97a808d8d13434117788
SMB Edition Squad <smbedition@prestashop.com>
2023-01-24 10:43:47 +0100

    chore: add test

--- packages/components/badge/test/badge.spec.ts
+++ packages/components/badge/test/badge.spec.ts
@@ -5,6 +5,9 @@ import type { MountingOptions, VueWrapper } from '@vue/test-utils'
 
 describe('Badge tests', () => {
   let wrapper: VueWrapper<any>
+
+  const findBadge = () => wrapper.find('.puik-badge')
+
   const factory = (
     propsData: Record<string, any> = {},
     options: MountingOptions<any> = {}
@@ -16,8 +19,14 @@ describe('Badge tests', () => {
       ...options,
     })
   }
+
   it('should be a vue instance', () => {
     factory()
     expect(wrapper).toBeTruthy()
+  })
+
+  it('should display an success badge by default', () => {
+    factory()
+    expect(findBadge().classes()).toContain('puik-badge--success')
   })
 })
