5eb2cb072980270577cf50a815eed7d80ebab615
Paleo <paleo@paroi.tech>
2023-01-26 11:51:24 +0100

    feat: [TCER-926] file upload component

--- packages/components/file-upload/index.ts
+++ packages/components/file-upload/index.ts
@@ -1,10 +1,10 @@
-import { withInstall } from '@puik/utils'
+import { withInstall, withNoopInstall } from '@puik/utils'
 import FileUpload from './src/file-upload.vue'
-// import FileUploadItem from './src/file-upload-item.vue'
+import FileUploadItem from './src/file-upload-item.vue'
 
 export const PuikFileUpload = withInstall(FileUpload)
 export default PuikFileUpload
 
-// export const PuikFileUploadItem = withInstall(FileUploadItem)
+export const PuikFileUploadItem = withNoopInstall(FileUploadItem)
 
 export * from './src/file-upload'
