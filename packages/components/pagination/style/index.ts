f6d6fed61f2b9365401eab4ae00cf3ba416dbe87
Aubin Mottier <56488255+Kelhel@users.noreply.github.com>
2023-02-21 14:44:07 +0100

    Update packages/components/pagination/style/index.ts
    Co-authored-by: André Amorim <46534068+aAmorim27@users.noreply.github.com>

--- packages/components/pagination/style/index.ts
+++ packages/components/pagination/style/index.ts
@@ -1,4 +1,4 @@
 import '@puik/components/base/style'
 import '@puik/theme/src/pagination.scss'
-import '@puik/theme/puik-button.css'
-import '@puik/theme/puik-select.css'
+import '@puik/theme/src/puik-button.scss'
+import '@puik/theme/src/puik-select.scss'
