b6b2f732b29aca5045821cb023fe6d526e32a7dd
André <andre.amorim@prestashop.com>
2023-03-17 11:37:08 +0100

    feat: added a standalone package for provider

---
+++ packages/provider/rollup.config.mjs
@@ -0,0 +1,34 @@
+// rollup.config.js
+import typescript from 'rollup-plugin-typescript2'
+import { defineConfig } from 'rollup'
+import pkg from './package.json' assert { type: 'json' }
+
+export default async () =>
+  defineConfig([
+    {
+      input: './index.ts',
+      external: [
+        ...Object.keys(pkg.dependencies),
+        ...Object.keys(pkg.peerDependencies),
+      ],
+      output: [
+        {
+          dir: './dist/es',
+          format: 'esm',
+          preserveModules: true,
+          entryFileNames: '[name].mjs',
+        },
+        {
+          dir: './dist/lib',
+          format: 'cjs',
+          preserveModules: true,
+          exports: 'named',
+        },
+      ],
+      plugins: [
+        typescript({
+          tsconfig: './tsconfig.build.json',
+        }),
+      ],
+    },
+  ])
