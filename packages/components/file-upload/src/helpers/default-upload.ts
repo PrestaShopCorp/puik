77b60fa4513dd3eef320619887da6cae9b2363be
Paleo <paleo@paroi.tech>
2023-04-06 14:22:14 +0200

    refactor: [TCER-926] upload files

--- packages/components/file-upload/src/helpers/default-upload.ts
+++ packages/components/file-upload/src/helpers/default-upload.ts
@@ -5,36 +5,30 @@ export const makeUpload = (url: string) => {
     file: File,
     options: { onUploadProgress?: UploadProgressHandler } = {}
   ): Promise<{ fileId: string }> => {
-    let resolve: (responseData: any) => void
-    let reject: (err: any) => void
-    const promise = new Promise<any>((resolveCb, rejectCb) => {
-      resolve = resolveCb
-      reject = rejectCb
+    const response = await new Promise<any>((resolve, reject) => {
+      const xhr = new XMLHttpRequest()
+      xhr.onerror = (error) => reject(error)
+      xhr.onreadystatechange = () => {
+        if (xhr.readyState === 4) {
+          resolve(xhr.response)
+        }
+      }
+      xhr.upload.onprogress = (event) => {
+        const progress = event.total === 0 ? 1 : event.loaded / event.total
+        options.onUploadProgress?.(progress)
+      }
+      xhr.open('POST', url)
+
+      const formData = new FormData()
+      formData.append('file', file)
+      xhr.send(formData)
     })
 
-    const xhr = new XMLHttpRequest()
-    xhr.onerror = (error) => reject(error)
-    xhr.onreadystatechange = () => {
-      if (xhr.readyState === 4) {
-        resolve(xhr.response)
-      }
-    }
-    xhr.upload.onprogress = (event) => {
-      const progress = event.total === 0 ? 1 : event.loaded / event.total
-      options.onUploadProgress?.(progress)
-    }
-    xhr.open('POST', url)
-
-    const formData = new FormData()
-    formData.append('file', file)
-    xhr.send(formData)
-
-    const responseData = await promise
-    if (!responseData) throw new Error(`Error ${responseData} from API`)
-    if (typeof responseData?.fileId !== 'string')
+    if (!response) throw new Error(`Error ${response} from API`)
+    if (typeof response?.fileId !== 'string')
       throw new Error('Invalid response from API')
     return {
-      fileId: responseData.fileId,
+      fileId: response.fileId,
     }
   }
   return upload
