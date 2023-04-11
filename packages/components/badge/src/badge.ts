77df76e8beedf7eed4cc4b16808fa51de45f5206
Aubin Mottier <aubin.mottier@gmail.com>
2023-04-11 15:11:49 +0200

    feat: add neutral variant

--- packages/components/badge/src/badge.ts
+++ packages/components/badge/src/badge.ts
@@ -2,7 +2,7 @@ import { buildProps } from '@puik/utils'
 import type { ExtractPropTypes } from 'vue'
 import type Badge from './badge.vue'
 
-export const badgeVariants = ['success', 'warning', 'danger', 'info']
+export const badgeVariants = ['success', 'warning', 'danger', 'info', 'neutral']
 
 export const badgeProps = buildProps({
   variant: {
