748c1d36f38704f265c2e95044f99228c494a1cf
jmcollin <jean-marie.collin@prestashop.com>
2023-05-31 14:04:00 +0200

    refactor: use svg as background

--- packages/components/spinner-loader/src/spinner-loader.vue
+++ packages/components/spinner-loader/src/spinner-loader.vue
@@ -7,48 +7,7 @@
       { 'puik-spinner-loader--right': position === 'right' },
     ]"
   >
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 200 200"
-      class="puik-spinner-loader__spinner"
-    >
-      <defs>
-        <linearGradient :id="`spinner-secondHalf--${size}--${variant}`">
-          <stop offset="0%" stop-opacity="1" stop-color="currentColor" />
-          <stop offset="100%" stop-opacity="0.5" stop-color="currentColor" />
-        </linearGradient>
-        <linearGradient :id="`spinner-firstHalf--${size}--${variant}`">
-          <stop offset="0%" stop-opacity="0" stop-color="currentColor" />
-          <stop offset="100%" stop-opacity="0.5" stop-color="currentColor" />
-        </linearGradient>
-      </defs>
-
-      <g stroke-width="14">
-        <path
-          :stroke="`url(#spinner-secondHalf--${size}--${variant})`"
-          d="M 8 100 A 32 32 0 0 1 190 100"
-        />
-        <path
-          :stroke="`url(#spinner-firstHalf--${size}--${variant})`"
-          d="M 190 100 A 32 32 0 0 1 8 100"
-        />
-        <path
-          stroke="currentColor"
-          stroke-linecap="round"
-          d="M 8 100 A 32 32 0 0 1 8 100"
-        />
-      </g>
-
-      <animateTransform
-        from="360 0 0"
-        to="0 0 0"
-        attributeName="transform"
-        type="rotate"
-        repeatCount="indefinite"
-        dur="2000ms"
-      />
-    </svg>
-
+    <div class="puik-spinner-loader__spinner bg-spinner"></div>
     <span v-if="label" class="puik-spinner-loader__label">
       {{ label }}
     </span>
--- packages/tailwind-preset/index.js
+++ packages/tailwind-preset/index.js
@@ -3,4 +3,18 @@ const Puiktheme = require('./theme')
 
 module.exports = {
   theme: Puiktheme,
+  plugins: [
+    function ({ addComponents, theme }) {
+      addComponents({
+        '.bg-spinner': {
+          backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 200 200' fill='none' color='${theme(
+            'colors.primary.800'
+          ).replace(
+            '#',
+            '%23'
+          )}' %3E%3Cdefs%3E%3ClinearGradient id='spinner-secondHalf'%3E%3Cstop offset='0%25' stop-opacity='1' stop-color='currentColor'%3E%3C/stop%3E%3Cstop offset='100%25' stop-opacity='0.5' stop-color='currentColor' %3E%3C/stop%3E%3C/linearGradient%3E%3ClinearGradient id='spinner-firstHalf'%3E%3Cstop offset='0%25' stop-opacity='0' stop-color='currentColor'%3E%3C/stop%3E%3Cstop offset='100%25' stop-opacity='0.5' stop-color='currentColor' %3E%3C/stop%3E%3C/linearGradient%3E%3C/defs%3E%3Cg stroke-width='14'%3E%3Cpath stroke='url(%23spinner-secondHalf)' d='M 8 100 A 32 32 0 0 1 190 100' %3E%3C/path%3E%3Cpath stroke='url(%23spinner-firstHalf)' d='M 190 100 A 32 32 0 0 1 8 100' %3E%3C/path%3E%3Cpath stroke='currentColor' stroke-linecap='round' d='M 8 100 A 32 32 0 0 1 8 100' %3E%3C/path%3E%3C/g%3E%3CanimateTransform from='360 0 0' to='0 0 0' attributeName='transform' type='rotate' repeatCount='indefinite' dur='2000ms' %3E%3C/animateTransform%3E%3C/svg%3E")`,
+        },
+      })
+    },
+  ],
 }
