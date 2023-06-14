4bac8d87fef322508ea6484d1039a991299935db
Aubin Mottier <aubin.mottier@gmail.com>
2023-06-14 14:25:41 +0200

    docs: update snippets

--- packages/components/table/stories/table.stories.ts
+++ packages/components/table/stories/table.stories.ts
@@ -93,10 +93,32 @@ export default {
     '`header-${header.value}`': {
       control: 'none',
       description: 'Slot to replace header',
+      table: {
+        type: {
+          summary: 'SlotProps',
+          detail: `
+{
+  header: PuikTableHeader
+  index: number
+}
+          `,
+        },
+      },
     },
     '`item-${header.value}`': {
       control: 'none',
       description: 'Slot to replace item',
+      table: {
+        type: {
+          summary: 'SlotProps',
+          detail: `
+{
+  item: any
+  index: number
+}
+          `,
+        },
+      },
     },
   },
   args: {
@@ -176,9 +198,212 @@ Default.parameters = {
   docs: {
     source: {
       code: `
-const headers: PuikTableHeader[] = ${headers}
 <!--VueJS Snippet-->
+const headers: PuikTableHeader[] = [
+  {
+    text: 'Nom',
+    value: 'lastname',
+    size: 'md',
+  },
+  {
+    text: 'Prénom',
+    value: 'firstname',
+    size: 'md',
+  },
+  {
+    text: 'Age',
+    value: 'age',
+    size: 'sm',
+    align: 'center',
+  },
+  {
+    text: 'Email',
+    value: 'email',
+    align: 'right',
+  },
+  {
+    value: 'actions',
+    size: 'sm',
+  },
+]
+
+<puik-table
+  v-model:selection="selection"
+  :headers="headers"
+  :items="items"
+>
+  <template #item-actions="{ item }">
+    <puik-button
+      variant="text"
+      right-icon="delete"
+      aria-label="Delete item"
+    ></puik-button>
+  </template>
+  <template #item-status="{ index }">
+    <puik-switch v-model="switches[index]"></puik-switch>
+  </template>
+</puik-table>
+
 <!--HTML/CSS Snippet-->
+<div class="puik-table__container">
+  <!--
+    Full width: "puik-table--full-width"
+
+    th :
+      size sm: "puik-table__head__row__item--sm"
+      size md: "puik-table__head__row__item--md"
+      size lg: "puik-table__head__row__item--lg"
+    td :
+      size sm: "puik-table__body__row__item--sm"
+      size md: "puik-table__body__row__item--md"
+      size lg: "puik-table__body__row__item--lg"
+
+      align left: "puik-table__body__row__item--left"
+      align center: "puik-table__body__row__item--center"
+      align right: "puik-table__body__row__item--right"
+  -->
+  <table class="puik-table">
+    <thead>
+      <tr>
+        <th class="puik-table__head__row__item--selection">
+          <div class="puik-checkbox puik-table__head__row__item--selection__checkbox">
+            <input id="puik-checkbox-6915" class="puik-checkbox__input" type="checkbox">
+            <label for="puik-checkbox-6915" class="puik-checkbox__label">Select all items</label>
+          </div>
+        </th>
+        <th>Nom</th>
+        <th>Prénom</th>
+        <th>Age</th>
+        <th>Status</th>
+        <th>Email</th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td class="puik-table__body__row__item--selection">
+          <div class="puik-checkbox puik-table__body__row__item--selection__checkbox">
+            <input id="puik-checkbox-7394" class="puik-checkbox__input" type="checkbox">
+            <label for="puik-checkbox-7394" class="puik-checkbox__label">Select item</label>
+          </div>
+        </td>
+        <td>Robel</td>
+        <td>Alberto</td>
+        <td>60</td>
+        <td>
+          <div class="puik-switch__group">
+            <button class="puik-switch" id="headlessui-switch-6" role="switch" type="button" tabindex="0" aria-checked="false" data-headlessui-state="">
+              <span class="puik-switch__toggle"></span>
+            </button>
+          </div>
+        </td>
+        <td>robel.alberto@email.com</td>
+        <td>
+          <button class="puik-button puik-button--text puik-button--md" aria-label="Delete item">
+            <div class="puik-icon material-icons-round puik-button__right-icon" style="font-size: 1.25rem;">delete</div>
+          </button>
+        </td>
+      </tr>
+      <tr>
+        <td class="puik-table__body__row__item--selection">
+          <div class="puik-checkbox puik-table__body__row__item--selection__checkbox">
+            <input id="puik-checkbox-3889" class="puik-checkbox__input" type="checkbox">
+            <label for="puik-checkbox-3889" class="puik-checkbox__label">Select item</label>
+          </div>
+        </td>
+        <td>Toy</td>
+        <td>Enos</td>
+        <td>55</td>
+        <td>
+          <div class="puik-switch__group">
+            <button class="puik-switch" id="headlessui-switch-7" role="switch" type="button" tabindex="0" aria-checked="false" data-headlessui-state="">
+              <span class="puik-switch__toggle"></span>
+            </button>
+          </div>
+        </td>
+        <td>toy.enos@email.com</td>
+        <td>
+          <button class="puik-button puik-button--text puik-button--md" aria-label="Delete item">
+            <div class="puik-icon material-icons-round puik-button__right-icon" style="font-size: 1.25rem;">delete</div>
+          </button>
+        </td>
+      </tr>
+      <tr>
+        <td class="puik-table__body__row__item--selection">
+          <div class="puik-checkbox puik-table__body__row__item--selection__checkbox">
+            <input id="puik-checkbox-282" class="puik-checkbox__input" type="checkbox">
+            <label for="puik-checkbox-282" class="puik-checkbox__label">Select item</label>
+          </div>
+        </td>
+        <td>Schumm</td>
+        <td>Hollie</td>
+        <td>21</td>
+        <td>
+          <div class="puik-switch__group">
+            <button class="puik-switch" id="headlessui-switch-8" role="switch" type="button" tabindex="0" aria-checked="false" data-headlessui-state="">
+              <span class="puik-switch__toggle"></span>
+            </button>
+          </div>
+        </td>
+        <td>schumm.hollie@email.com</td>
+        <td>
+          <button class="puik-button puik-button--text puik-button--md" aria-label="Delete item">
+            <div class="puik-icon material-icons-round puik-button__right-icon" style="font-size: 1.25rem;">delete</div>
+          </button>
+        </td>
+      </tr>
+      <tr>
+        <td class="puik-table__body__row__item--selection">
+          <div class="puik-checkbox puik-table__body__row__item--selection__checkbox">
+            <input id="puik-checkbox-7691" class="puik-checkbox__input" type="checkbox">
+            <label for="puik-checkbox-7691" class="puik-checkbox__label">Select item</label>
+          </div>
+        </td>
+        <td>DuBuque</td>
+        <td>Earnestine</td>
+        <td>86</td>
+        <td>
+          <div class="puik-switch__group">
+            <button class="puik-switch" id="headlessui-switch-9" role="switch" type="button" tabindex="0" aria-checked="false" data-headlessui-state="">
+              <span class="puik-switch__toggle"></span>
+            </button>
+          </div>
+        </td>
+        <td>dubuque.earnestine@email.com</td>
+        <td>
+          <button class="puik-button puik-button--text puik-button--md" aria-label="Delete item">
+            <!--v-if-->
+            <div class="puik-icon material-icons-round puik-button__right-icon" style="font-size: 1.25rem;">delete</div>
+          </button>
+        </td>
+      </tr>
+      <tr>
+        <td class="puik-table__body__row__item--selection">
+          <div class="puik-checkbox puik-table__body__row__item--selection__checkbox">
+            <input id="puik-checkbox-2613" class="puik-checkbox__input" type="checkbox">
+            <label for="puik-checkbox-2613" class="puik-checkbox__label">Select item</label>
+          </div>
+        </td>
+        <td>Mann</td>
+        <td>Hadley</td>
+        <td>41</td>
+        <td>
+          <div class="puik-switch__group">
+            <button class="puik-switch" id="headlessui-switch-10" role="switch" type="button" tabindex="0" aria-checked="false" data-headlessui-state="">
+              <span class="puik-switch__toggle"></span>
+            </button>
+          </div>
+        </td>
+        <td>mann.hadley@email.com</td>
+        <td>
+          <button class="puik-button puik-button--text puik-button--md" aria-label="Delete item">
+            <div class="puik-icon material-icons-round puik-button__right-icon" style="font-size: 1.25rem;">delete</div>
+          </button>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</div>
       `,
       language: 'html',
     },
