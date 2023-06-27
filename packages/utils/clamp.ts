52abd87176e90cea79626d1dd9105211a2fc1fa8
Guillaume Guerin <guillaume.guerin@prestashop.com>
2023-06-27 10:00:10 +0200

    fix: add clamp comment

--- packages/utils/clamp.ts
+++ packages/utils/clamp.ts
@@ -1,2 +1,8 @@
+/*
+Limit number to min and max range
+clamp(50, 1, 100) => 50
+clamp(-2, 1, 100) => 1
+clamp(250, 1, 100) => 100
+*/
 export const clamp = (number: number, min: number, max: number) =>
   Math.max(min, Math.min(max, number))
