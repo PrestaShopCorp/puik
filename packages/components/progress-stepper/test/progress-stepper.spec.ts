fdf0c4b61b725e0d6e5f5da543e6273a3bb82b32
Matthias Goudjil <matthias.goudjil@prestashop.com>
2023-07-31 17:15:47 +0200

    test: add tests for progress-stepper

--- packages/components/progress-stepper/test/progress-stepper.spec.ts
+++ packages/components/progress-stepper/test/progress-stepper.spec.ts
@@ -1,23 +1,79 @@
+import { nextTick, ref } from 'vue'
 import { mount } from '@vue/test-utils'
 import { describe, it, expect } from 'vitest'
 import PuikProgressStepper from '../src/progress-stepper.vue'
-import type { MountingOptions, VueWrapper } from '@vue/test-utils'
+import PuikProgressStepperStep from '../src/progress-stepper-step.vue'
+import type { VueWrapper } from '@vue/test-utils'
 
 describe('ProgressStepper tests', () => {
   let wrapper: VueWrapper<any>
-  const factory = (
-    propsData: Record<string, any> = {},
-    options: MountingOptions<any> = {}
-  ) => {
-    wrapper = mount(PuikProgressStepper, {
-      props: {
-        ...propsData,
+  const factory = () => {
+    wrapper = mount({
+      template: `
+      <puik-progress-stepper v-model="currentStep">
+        <puik-progress-stepper-step :step="1">
+          <template #text>Text step 1</template>
+          <template #additional-text>Additional text step 1</template>
+        </puik-progress-stepper-step>
+        <puik-progress-stepper-step :step="2">
+          <template #text>Text step 1</template>
+          <template #additional-text>Additional text step 1</template>
+        </puik-progress-stepper-step>
+        <puik-progress-stepper-step :step="3">
+          <template #text>Text step 1</template>
+          <template #additional-text>Additional text step 1</template>
+        </puik-progress-stepper-step>
+      </puik-progress-stepper>
+      `,
+      components: {
+        PuikProgressStepper,
+        PuikProgressStepperStep,
       },
-      ...options,
+      setup() {
+        const currentStep = ref(2)
+        return { currentStep }
+      },
     })
   }
+  const getSteps = () => wrapper.findAllComponents(PuikProgressStepperStep)
+  const getStepButton = (step) => step.find('.puik-button')
   it('should be a vue instance', () => {
     factory()
     expect(wrapper).toBeTruthy()
+  })
+  it('should have 3 steps', () => {
+    factory()
+    const steps = getSteps()
+    expect(steps.length).toBe(3)
+  })
+  it('should first step to be success', async () => {
+    factory()
+    await nextTick()
+    const steps = getSteps()
+    console.log(getStepButton(steps[2]).classes())
+    expect(getStepButton(steps[0]).classes()).toContain('puik-button--success')
+  })
+  it('should second step be current step', async () => {
+    factory()
+    await nextTick()
+    const steps = getSteps()
+    expect(getStepButton(steps[1]).classes()).toContain('puik-button--primary')
+  })
+  it('should third step to be disabled', async () => {
+    factory()
+    await nextTick()
+    const steps = getSteps()
+    const step = getStepButton(steps[2])
+    expect(step.classes()).toContain('puik-button--primary')
+    expect(step.attributes('disabled')).toBeFalsy()
+  })
+  it('should step emit click event', async () => {
+    factory()
+    await nextTick()
+    const steps = getSteps()
+    const step = steps[0]
+    const stepButton = getStepButton(step)
+    await stepButton.trigger('click')
+    expect(step.emitted('click')).toBeTruthy()
   })
 })
