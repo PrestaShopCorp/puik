3280cd35e9637672ffc6c4e6e77c4549364fa8c4
Matthias Goudjil <matthias.goudjil@prestashop.com>
2023-08-09 16:39:52 +0200

    feat: [tab-navigation] - handle keyboard events to switch panels

--- packages/components/tab-navigation/src/tab-navigation.vue
+++ packages/components/tab-navigation/src/tab-navigation.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="puik-tab-navigation">
+  <div :id="name" class="puik-tab-navigation" @keyup="handleKeysEvent">
     <slot />
   </div>
 </template>
 
 <script setup lang="ts">
-import { provide, ref } from 'vue'
+import { nextTick, provide, ref } from 'vue'
 import { currentTabKey, tabNavigationProps } from './tab-navigation'
 
 defineOptions({
@@ -14,7 +14,7 @@ defineOptions({
 
 const props = defineProps(tabNavigationProps)
 
-const name = ref(props.name)
+const name = ref<string>(props.name)
 const currentPosition = ref<number>(props.defaultPosition)
 const handleTabClick = (index: number) => {
   currentPosition.value = index
@@ -25,4 +25,46 @@ provide(currentTabKey, {
   currentPosition,
   handleTabClick,
 })
+
+const selectNextTab = () => {
+  const tabNavigationInstance = document.querySelector(`#${name.value}`)
+  const numberOfTabs =
+    tabNavigationInstance?.querySelectorAll('[role="tab"]').length || 1
+
+  currentPosition.value === numberOfTabs
+    ? (currentPosition.value = 1)
+    : currentPosition.value++
+
+  nextTick(() => {
+    const tabSelected = tabNavigationInstance?.querySelector(
+      '.puik-tab-navigation__title--selected'
+    ) as HTMLElement
+    tabSelected.focus()
+  })
+}
+
+const selectPreviousTab = () => {
+  const tabNavigationInstance = document.querySelector(`#${name.value}`)
+  const numberOfTabs =
+    tabNavigationInstance?.querySelectorAll('[role="tab"]').length || 1
+
+  currentPosition.value === 1
+    ? (currentPosition.value = numberOfTabs)
+    : currentPosition.value--
+
+  nextTick(() => {
+    const tabSelected = tabNavigationInstance?.querySelector(
+      '.puik-tab-navigation__title--selected'
+    ) as HTMLElement
+    tabSelected.focus()
+  })
+}
+
+const handleKeysEvent = (event: KeyboardEvent) => {
+  if (event.key === 'ArrowRight') {
+    selectNextTab()
+  } else if (event.key === 'ArrowLeft') {
+    selectPreviousTab()
+  }
+}
 </script>
