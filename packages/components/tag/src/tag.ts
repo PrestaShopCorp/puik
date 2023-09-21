459d1451cf76e109b10c53639555e9e8214f69b9
Matthias Goudjil <matthias.goudjil@prestashop.com>
2023-09-21 17:29:47 +0200

    fix: #191 tag-component

--- packages/components/tag/src/tag.ts
+++ packages/components/tag/src/tag.ts
@@ -1,4 +1,5 @@
 import { buildProps } from '@puik/utils'
+import type { PuikTooltipPosition } from '@puik/components'
 import type { ExtractPropTypes, PropType } from 'vue'
 import type Tag from './tag.vue'
 
@@ -10,18 +11,31 @@ export const tagColorsVariants = [
   'purple',
 ] as const
 
+export const tagSizeVariants = ['default', 'small'] as const
+
 export type PuikTagColorVariant = (typeof tagColorsVariants)[number]
+export type PuikTagSizeVariant = (typeof tagSizeVariants)[number]
 
 export const tagProps = buildProps({
   id: {
     type: String,
     required: true,
-    default: '',
+    default: undefined,
+  },
+  content: {
+    type: String,
+    required: true,
+    default: undefined,
   },
   variant: {
-    type: String as PropType<PuikTagColorVariant>,
+    type: String,
     required: false,
     default: 'neutral',
+  },
+  size: {
+    type: String,
+    required: false,
+    default: 'default',
   },
   icon: {
     type: String,
@@ -31,12 +45,17 @@ export const tagProps = buildProps({
   closeable: {
     type: Boolean,
     required: false,
-    default: true,
+    default: false,
   },
   disabled: {
     type: Boolean,
     required: false,
     default: false,
+  },
+  tooltipPosition: {
+    type: String as PropType<PuikTooltipPosition>,
+    Required: false,
+    default: 'bottom',
   },
 } as const)
 
