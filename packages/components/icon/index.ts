3e5baf0be96b6ee6bc93a108268ce1dcd52301e1
Matthias Goudjil <matthias.goudjil@prestashop.com>
2023-09-25 16:00:57 +0200

    build: define custom elements in puik bundle

--- packages/components/icon/index.ts
+++ packages/components/icon/index.ts
@@ -6,6 +6,8 @@ import Icon from './src/icon.vue'
 export const PuikIcon = withInstall(Icon)
 export const PuikIconCe = defineCustomElement(PuikIcon)
 
+customElements.define('puik-icon-ce', PuikIconCe)
+
 export * from './src/icon'
 
 export default { PuikIcon, PuikIconCe }
