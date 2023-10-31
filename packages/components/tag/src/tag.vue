29e76eefc9d168238134a9dafd0565a4f1274a9c
Guillaume Lepoetre <guillaume.lepoetre@prestashop.com>
2023-10-31 17:34:37 +0100

    feat: add data-test to PuikTag

--- packages/components/tag/src/tag.vue
+++ packages/components/tag/src/tag.vue
@@ -7,11 +7,12 @@
     ]"
   >
     <PuikIcon v-if="icon && icon != ''" :icon="icon" class="puik-tag__icon" />
-    <div class="puik-tag__content">
+    <div class="puik-tag__content" :data-test="`content-${dataTest}`">
       <puik-tooltip
         v-if="content?.length >= 30"
         :position="(tooltipPosition as PuikTooltipPosition)"
         :description="content"
+        :data-test="`tooltip-${dataTest}`"
       >
         <template #description>{{ content }}</template>
         {{ content }}
