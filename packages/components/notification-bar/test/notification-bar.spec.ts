af7fb88e52ce90c1982a83e09426e0c024e682bf
Matthias Goudjil <matthias.goudjil@prestashop.com>
2024-02-15 09:20:31 +0100

    test: add notification-bar tests

--- packages/components/notification-bar/test/notification-bar.spec.ts
+++ packages/components/notification-bar/test/notification-bar.spec.ts
@@ -11,6 +11,7 @@ describe('NotificationBar tests', () => {
   ) => {
     wrapper = mount(PuikNotificationBar, {
       props: {
+        messages: [{ text: 'Default message' }],
         ...propsData,
       },
       ...options,
@@ -19,5 +20,58 @@ describe('NotificationBar tests', () => {
   it('should be a vue instance', () => {
     factory()
     expect(wrapper).toBeTruthy()
+  })
+
+  it('should accept teleport prop', () => {
+    const teleport = { to: 'body', prepend: true }
+    factory({ teleport })
+    expect(wrapper.props().teleport).toEqual(teleport)
+  })
+
+  it('should accept variant prop', () => {
+    const variant = 'blue'
+    factory({ variant })
+    expect(wrapper.props().variant).toBe(variant)
+  })
+
+  it('should accept closable prop', () => {
+    const closable = true
+    factory({ closable })
+    expect(wrapper.props().closable).toBe(closable)
+  })
+
+  it('should accept messages prop', () => {
+    const messages = [
+      {
+        icon: 'info',
+        text: 'This is a test message',
+        link: { url: 'https://test.com', text: 'Test Link' },
+      },
+    ]
+    factory({ messages })
+    expect(wrapper.props().messages).toEqual(messages)
+  })
+
+  it('should emit close event when close button is clicked', async () => {
+    factory({
+      closable: true,
+      messages: [{ text: 'This is a test message' }],
+    })
+    await wrapper.find('.notification-bar__close-button').trigger('click')
+    expect(wrapper.emitted()).toHaveProperty('close')
+  })
+
+  it('should have correct classes based on props', () => {
+    factory({
+      variant: 'blue',
+      closable: true,
+      messages: [{ text: 'This is a test message' }],
+    })
+    expect(wrapper.find('.notification-bar__container').classes()).toContain(
+      'notification-bar__container--blue'
+    )
+    expect(wrapper.find('.notification-bar__container').classes()).toContain(
+      'notification-bar__container--closable'
+    )
   })
 })
