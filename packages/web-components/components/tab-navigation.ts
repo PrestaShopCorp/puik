bad25ba8d1915e25027d5ad4a04d944c9e2ac97d
mattgoud <matthias.goudjil@prestashop.com>
2025-01-17 14:21:29 +0100

    chore: related #411 - fix TS error on web-component tab-navigation build

--- packages/web-components/components/tab-navigation.ts
+++ packages/web-components/components/tab-navigation.ts
@@ -1,8 +1,8 @@
-import { defineCustomElement } from 'vue';
+import { DefineComponent, defineCustomElement } from 'vue';
 import { PuikTabNavigation } from '@prestashopcorp/puik-components';
 import type { CustomElementWithName } from '../types';
 
-const PuikTabNavigationCe = defineCustomElement(PuikTabNavigation) as CustomElementWithName;
+const PuikTabNavigationCe = defineCustomElement(PuikTabNavigation as DefineComponent<unknown, any, any, any>) as CustomElementWithName;
 PuikTabNavigationCe.ceName = 'puik-tab-navigation-ce';
 
 export default PuikTabNavigationCe;
