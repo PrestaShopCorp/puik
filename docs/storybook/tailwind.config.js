7236a637d2cc1bc32cc835b063418d04c0286646
mattgoud <matthias.goudjil@prestashop.com>
2025-02-12 17:35:15 +0100

    feat: [Vitepress-doc] - related #416 - change structure of docs directory

--- docs/storybook/tailwind.config.js
+++ docs/storybook/tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   presets: [require('@prestashopcorp/puik-theme/tailwind.config')],
   content: [
-    '../packages/components/**/*.stories.@(js|jsx|ts|tsx)',
+    '../../packages/components/**/*.stories.@(js|jsx|ts|tsx)',
     './stories/**/*.@(mdx|stories.@(js|jsx|ts|tsx))'
   ]
 };
