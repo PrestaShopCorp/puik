aaf62c522788d10de249fe1ce8c418b5ad63a9ee
mattgoud <matthias.goudjil@prestashop.com>
2025-02-28 11:31:07 +0100

    fix: [puik-component-build] - add missing puik-group-options scss file, fix typo (scss import for pagination)

--- packages/components/pagination/style/index.ts
+++ packages/components/pagination/style/index.ts
@@ -1,4 +1,4 @@
 import '@prestashopcorp/puik-components/base/style';
 import '@prestashopcorp/puik-theme/src/puik-pagination.scss';
-import '@prestashopcorp/puik-theme/src/puik-puik-button.scss';
-import '@prestashopcorp/puik-theme/src/puik-puik-select.scss';
+import '@prestashopcorp/puik-theme/src/puik-button.scss';
+import '@prestashopcorp/puik-theme/src/puik-select.scss';
