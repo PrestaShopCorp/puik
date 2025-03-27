b6035d93160dce9bcf7945fbf3c962ba9c6e03e2
mattgoud <matthias.goudjil@prestashop.com>
2025-03-27 12:04:30 +0100

    chore: related#424 - [storybook]  add preview script, [puik-components] improve vite.config syntax

--- packages/components/vite.config.ts
+++ packages/components/vite.config.ts
@@ -1,4 +1,4 @@
-import { resolve } from 'path';
+import { resolve as pathResolve } from 'path';
 import glob from 'fast-glob';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
@@ -6,6 +6,15 @@ import { nodeResolve } from '@rollup/plugin-node-resolve';
 import vue from '@vitejs/plugin-vue';
 import { excludeFiles } from '../utils';
 import pkg from './package.json' assert { type: 'json' };
+
+const inputFiles = excludeFiles(
+  await glob('./**/*.{vue,ts}', {
+    cwd: './',
+    absolute: true,
+    onlyFiles: true,
+  }),
+  ['stories', 'test']
+);
 
 export default defineConfig({
   plugins: [
@@ -16,70 +25,48 @@ export default defineConfig({
       tsconfigPath: 'tsconfig.build.json',
     }),
     nodeResolve({
-      mainFields: ['module', 'js', 'json'],
+      mainFields: ['module', 'main'],
     }),
   ],
   build: {
-    cssCodeSplit: true,
     lib: {
-      entry: resolve(__dirname, './index.ts'),
+      entry: pathResolve(__dirname, './index.ts'),
     },
     rollupOptions: {
       external: [
-        ...Object.keys(pkg.dependencies),
-        ...Object.keys(pkg.peerDependencies),
+        ...Object.keys(pkg.dependencies ?? {}),
+        ...Object.keys(pkg.peerDependencies ?? {}),
       ],
-      input: excludeFiles(
-        await glob('./**/*.{vue,ts}', {
-          cwd: './',
-          absolute: true,
-          onlyFiles: true,
-        }),
-        ['stories', 'test']
-      ),
+      input: inputFiles,
       output: [
         {
           dir: './dist',
           format: 'esm',
           preserveModules: true,
-          preserveModulesRoot: resolve(__dirname, './'),
-          entryFileNames: (chunk) => {
-            if (chunk.name.includes('node_modules')) {
-              return `${chunk.name.replace('node_modules', '_external')}.mjs`;
-            }
-            if (chunk.name.includes('packages')) {
-              return `${chunk.name.replace('packages', '_external')}.mjs`;
-            }
-            return '[name].mjs';
-          },
+          preserveModulesRoot: pathResolve(__dirname, './'),
+          entryFileNames: (chunk) => chunk.name.includes('node_modules')
+            ? chunk.name.replace('node_modules', '_external') + '.mjs'
+            : '[name].mjs',
           exports: 'named',
-          assetFileNames: 'style/[name].[ext]'
         },
         {
           dir: './dist',
           format: 'cjs',
           preserveModules: true,
-          preserveModulesRoot: resolve(__dirname, './'),
-          entryFileNames: (chunk) => {
-            if (chunk.name.includes('node_modules')) {
-              return `${chunk.name.replace('node_modules', '_external')}.cjs`;
-            }
-            if (chunk.name.includes('packages')) {
-              return `${chunk.name.replace('packages', '_external')}.cjs`;
-            }
-            return '[name].cjs';
-          },
+          preserveModulesRoot: pathResolve(__dirname, './'),
+          entryFileNames: (chunk) => chunk.name.includes('node_modules')
+            ? chunk.name.replace('node_modules', '_external') + '.cjs'
+            : '[name].cjs',
           exports: 'named',
-          assetFileNames: 'style/[name].[ext]'
         },
       ],
     },
   },
   resolve: {
     alias: {
-      '@prestashopcorp/puik-theme/assets': resolve(__dirname, './node_modules/@prestashopcorp/puik-theme/assets'),
-      '@prestashopcorp/puik-theme/src': resolve(__dirname, './node_modules/@prestashopcorp/puik-theme/src'),
-      '@prestashopcorp/puik-theme': resolve(__dirname, './node_modules/@prestashopcorp/puik-theme/dist'),
+      '@prestashopcorp/puik-theme/assets': pathResolve(__dirname, './node_modules/@prestashopcorp/puik-theme/assets'),
+      '@prestashopcorp/puik-theme/src': pathResolve(__dirname, './node_modules/@prestashopcorp/puik-theme/src'),
+      '@prestashopcorp/puik-theme': pathResolve(__dirname, './node_modules/@prestashopcorp/puik-theme/dist'),
     },
   },
 });
