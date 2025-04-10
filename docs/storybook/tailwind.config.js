70cc865e05f7113f10f1b18d6b6ffcc7d694595b
mattgoud <matthias.goudjil@prestashop.com>
2025-04-10 15:00:49 +0200

    chore: related #437 - [theme] add puik-tailwind-preset, [package.json] add licence and author attributes

--- docs/storybook/tailwind.config.js
+++ docs/storybook/tailwind.config.js
@@ -1,5 +1,6 @@
+const { puikTailwindPreset } = require('@prestashopcorp/puik-tailwind-preset');
 module.exports = {
-  presets: [require('@prestashopcorp/puik-theme/tailwind.config')],
+  presets: [puikTailwindPreset],
   content: [
     '../../packages/components/**/*.stories.@(js|jsx|ts|tsx)',
     './stories/**/*.@(mdx|stories.@(js|jsx|ts|tsx))'
