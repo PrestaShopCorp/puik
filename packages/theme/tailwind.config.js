bd034b907a53c6dbc0f57106760475392548f85e
mattgoud <matthias.goudjil@prestashop.com>
2025-04-29 19:00:53 +0200

    feat: [migration-tailwind-v4] - remove scss (wip)

--- packages/theme/tailwind.config.js
+++ packages/theme/tailwind.config.js
@@ -1,6 +1,4 @@
-const { puikTailwindPreset } = require('@prestashopcorp/puik-tailwind-preset');
 module.exports = {
-  presets: [puikTailwindPreset],
-  content: ['./src/**/*.scss'],
-  darkMode: 'class'
+  // content: ['./src/**/*.scss']
+  content: ['./dist/*.css']
 };
