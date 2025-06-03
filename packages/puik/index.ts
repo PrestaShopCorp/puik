8839a19d1089916cd680f1d6f0fb3037df05f918
mattgoud <matthias.goudjil@prestashop.com>
2025-06-03 15:33:11 +0200

    chore: [puik-tailwind-preset] - remove export of puikTailwindPreset from puik global package + update readme

--- packages/puik/index.ts
+++ packages/puik/index.ts
@@ -1,6 +1,5 @@
 import installer from './defaults';
 export * from '@prestashopcorp/puik-components';
 export { PuikResolver } from '@prestashopcorp/puik-resolver';
-export { puikTailwindPreset } from '@prestashopcorp/puik-tailwind-preset';
 
 export default installer;
